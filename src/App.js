5b7bdb8a64f9fe80a05e73e5912bbb6985df30e8
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-05 17:04:51 +0100

    created a serch feature

--- src/App.js
+++ src/App.js
@@ -7,25 +7,56 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters:[],
+      monsters: [],
+      searchField: "",
     };
   }
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then((users) => this.setState(() => {
-      return { monsters: users}
-    }, () => {
-      console.log(this.state)
-    })
-    )
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(
+        searchField.toLocaleLowerCase()
+      );
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster)=> {
-          return <h1>{monster.name}</h1>
-         
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search-monster"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
@@ -33,3 +64,42 @@ class App extends Component {
 }
 
 export default App;
+
+
+// import { useState, useEffect } from "react";
+
+// const App = () => {
+//   const [monsters, setMonsters] = useState([]);
+//   const [searchField, setSearchField] = useState("");
+
+//   useEffect(() => {
+//     fetch("http://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => setMonsters(users));
+//   }, []);
+
+//   const filteredMonsters = monsters.filter((monster) =>
+//     monster.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase())
+//   );
+
+//   return (
+//     <div className="App">
+//       <input
+//         className="search-box"
+//         type="search"
+//         placeholder="search-monster"
+//         onChange={(event) => {
+//           console.log(event.target.value);
+//           setSearchField(event.target.value);
+//         }}
+//       />
+//       {filteredMonsters.map((monster) => (
+//         <div key={monster.id}>
+//           <h1>{monster.name}</h1>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default App;
